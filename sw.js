c41dfe7749c0ea4664dd10f5d5aaf307880035e7
duturaev <duturaev@gmail.com>
2026-06-13 16:23:26 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'mamura-health-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'mamura-health-v2';
+const ASSETS = [
+  '/pressure_pils/',
+  '/pressure_pils/index.html',
+  '/pressure_pils/manifest.json',
+  '/pressure_pils/icons/icon-192.png',
+  '/pressure_pils/icons/icon-512.png',
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
